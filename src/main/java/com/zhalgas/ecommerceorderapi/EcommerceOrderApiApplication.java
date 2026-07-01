f0a059c4461f183339cbb4eece786041e2e612c6
zhalgasadil <zhalgasvactor@gmail.com>
2026-07-01 15:06:03 +0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/zhalgas/ecommerceorderapi/EcommerceOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.zhalgas.ecommerceorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceOrderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhalgas/ecommerceorderapi/EcommerceOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhalgas.ecommerceorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
