a8c95b4b775a4709d7f7ab2e2e56a2a833d66197
zhalgasadil <zhalgasvactor@gmail.com>
2026-07-02 16:31:34 +0500

    Add ResourceNotFoundException class for handling resource not found errors

--- src/main/java/com/zhalgas/ecommerceorderapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/zhalgas/ecommerceorderapi/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.zhalgas.ecommerceorderapi.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
