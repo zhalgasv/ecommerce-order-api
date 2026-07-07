f3549b51792c73af5c51e4de6b264d761b93ae3a
zhalgasadil <zhalgasvactor@gmail.com>
2026-07-07 17:47:31 +0500

    Add OrderController and OrderService to retrieve order details by ID

--- src/main/java/com/zhalgas/ecommerceorderapi/order/OrderController.java
+++ src/main/java/com/zhalgas/ecommerceorderapi/order/OrderController.java
@@ -1,4 +1,24 @@
 package com.zhalgas.ecommerceorderapi.order;
 
+import com.zhalgas.ecommerceorderapi.order.dto.OrderResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
+    @GetMapping("/{orderId}")
+    public OrderResponse getOrderById(@PathVariable Long orderId) {
+        return orderService.getOrderById(orderId);
+    }
 }
--- src/main/java/com/zhalgas/ecommerceorderapi/order/OrderService.java
+++ src/main/java/com/zhalgas/ecommerceorderapi/order/OrderService.java
@@ -1,4 +1,27 @@
 package com.zhalgas.ecommerceorderapi.order;
 
+import com.zhalgas.ecommerceorderapi.exception.ResourceNotFoundException;
+import com.zhalgas.ecommerceorderapi.order.dto.OrderResponse;
+import com.zhalgas.ecommerceorderapi.order.mapper.OrderMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+
+
+    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper) {
+        this.orderRepository = orderRepository;
+        this.orderMapper = orderMapper;
+    }
+
+    public OrderResponse getOrderById(Long orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order Not Found with id: " + orderId));
+        return orderMapper.toOrderResponse(order);
+    }
+
+
 }
