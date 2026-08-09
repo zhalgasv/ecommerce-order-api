a1e6a121cbd297fcc5f2624bb1c02b4e37ab032a
zhalgasadil <zhalgasvactor@gmail.com>
2026-08-09 15:57:39 +0500

    Enhance JwtService to validate token expiration in isTokenValid method

--- src/main/java/com/zhalgas/ecommerceorderapi/security/JwtService.java
+++ src/main/java/com/zhalgas/ecommerceorderapi/security/JwtService.java
@@ -40,6 +40,19 @@ public class JwtService {
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         String username = extractUsername(token);
-        return username.equals(userDetails.getUsername());
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
     }
 }
