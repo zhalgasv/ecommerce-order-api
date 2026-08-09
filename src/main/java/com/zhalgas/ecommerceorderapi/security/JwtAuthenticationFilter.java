6bfdba922192be26acdc72a2cbce501cb0ff91c8
zhalgasadil <zhalgasvactor@gmail.com>
2026-08-10 00:47:21 +0500

    Implement JwtAuthenticationFilter to handle JWT authentication in the request pipeline

--- src/main/java/com/zhalgas/ecommerceorderapi/security/JwtAuthenticationFilter.java
+++ src/main/java/com/zhalgas/ecommerceorderapi/security/JwtAuthenticationFilter.java
@@ -1,4 +1,38 @@
 package com.zhalgas.ecommerceorderapi.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, CustomUserDetailsService customUserDetailsService) {
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
 }
