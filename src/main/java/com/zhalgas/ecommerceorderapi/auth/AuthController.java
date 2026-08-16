2e84c85c76aea950ce86f375afcb1cd66e4321da
zhalgasadil <zhalgasvactor@gmail.com>
2026-08-16 19:22:36 +0500

    Add AuthController with registration and login endpoints, and implement AuthControllerTest

--- src/main/java/com/zhalgas/ecommerceorderapi/auth/AuthController.java
+++ src/main/java/com/zhalgas/ecommerceorderapi/auth/AuthController.java
@@ -1,4 +1,35 @@
 package com.zhalgas.ecommerceorderapi.auth;
 
+import com.zhalgas.ecommerceorderapi.auth.dto.AuthResponse;
+import com.zhalgas.ecommerceorderapi.auth.dto.LoginRequest;
+import com.zhalgas.ecommerceorderapi.auth.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public AuthResponse register(
+            @Valid @RequestBody RegisterRequest registerRequest
+    ) {
+        return authService.register(registerRequest);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(
+            @Valid @RequestBody LoginRequest loginRequest
+    ) {
+        return authService.login(loginRequest);
+    }
 }
---
+++ src/test/java/com/zhalgas/ecommerceorderapi/auth/AuthControllerTest.java
@@ -0,0 +1,52 @@
+package com.zhalgas.ecommerceorderapi.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zhalgas.ecommerceorderapi.auth.dto.AuthResponse;
+import com.zhalgas.ecommerceorderapi.auth.dto.LoginRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AuthService authService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void login_whenRequestIsValid_returnsToken() throws Exception {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("email");
+        loginRequest.setPassword("password");
+
+        AuthResponse response = new AuthResponse("jwt-token");
+
+        when(authService.login(any(LoginRequest.class))).thenReturn(response);
+
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value("jwt-token"));
+
+        verify(authService).login(any(LoginRequest.class));
+    }
+}
